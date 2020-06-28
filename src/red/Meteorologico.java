e7863e887e4193d393d79c4b519dd8ab45f70f19
Leandro Ciccarone <lciccarone@vuce.gob.ar>
2020-06-28 18:07:34 -0300

    soy un salame y deje copiada y pegada la funcion

--- src/red/Meteorologico.java
+++ src/red/Meteorologico.java
@@ -9,26 +9,9 @@ public class Meteorologico extends Satelite{
     }
 
     public void recibirMensaje(PingRequest mensaje) {
-        if (mensaje.getDestino().getId() == this.getId()) {
-            PingReply respuesta = new PingReply(mensaje.getId(), this, mensaje.getCreador(), new contenido.PingReply(0));
-            this.enviarMensaje(respuesta);
-        } else if (mensaje.getContenido().getReenvio()) {
-            mensaje.setOrigen(this);
-            mensaje.addOperadorRecorrido(this);
-            this.reenviarMensaje(mensaje);
-        }
-        this.getConsola().add(mensaje);
+
     }
 
     public void recibirMensaje(PingReply mensaje) {
-        if (mensaje.getDestino().getId() == this.getId()) {
-            System.out.println(mensaje.getContenido());
-        } else if (mensaje.getContenido().getReenvio()) {
-            mensaje.setOrigen(this);
-            mensaje.addOperadorRecorrido(this);
-            this.reenviarMensaje(mensaje);
-
-        }
-        this.getConsola().add(mensaje);
     }
 }
